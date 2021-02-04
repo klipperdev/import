3cac0aac3328a97e515cf274f0570e5e6c70fb1f
François Pluchino <francois.pluchino@klipper.dev>
2021-02-04 17:48:48 +0100

    Add doctrine listener to avoid to delete imports in progress

---
+++ Doctrine/Listener/ImportSubscriber.php
@@ -0,0 +1,62 @@
+<?php
+
+/*
+ * This file is part of the Klipper package.
+ *
+ * (c) François Pluchino <francois.pluchino@klipper.dev>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Klipper\Component\Import\Doctrine\Listener;
+
+use Doctrine\Common\EventSubscriber;
+use Doctrine\ORM\Event\OnFlushEventArgs;
+use Doctrine\ORM\Events;
+use Klipper\Component\DoctrineExtensionsExtra\Util\ListenerUtil;
+use Klipper\Component\Import\Model\ImportInterface;
+use Symfony\Contracts\Translation\TranslatorInterface;
+
+/**
+ * Doctrine subscriber for the import model.
+ *
+ * @author François Pluchino <francois.pluchino@klipper.dev>
+ */
+class ImportSubscriber implements EventSubscriber
+{
+    private TranslatorInterface $translator;
+
+    public function __construct(TranslatorInterface $translator)
+    {
+        $this->translator = $translator;
+    }
+
+    public function getSubscribedEvents(): array
+    {
+        return [
+            Events::onFlush,
+        ];
+    }
+
+    /**
+     * On flush action.
+     */
+    public function onFlush(OnFlushEventArgs $args): void
+    {
+        $em = $args->getEntityManager();
+        $uow = $em->getUnitOfWork();
+
+        foreach ($uow->getScheduledEntityDeletions() as $entity) {
+            if ($entity instanceof ImportInterface) {
+                if ('in_progress' === $entity->getStatus()) {
+                    ListenerUtil::thrownError($this->translator->trans(
+                        'klipper_import.orm_listener.import_in_progress',
+                        [],
+                        'validators'
+                    ));
+                }
+            }
+        }
+    }
+}
